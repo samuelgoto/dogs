0fd5a8fac9f995788268ec0d50ae8dd20ecea685
samuelgoto@gmail.com <samuelgoto@gmail.com>
2018-09-20 14:18:07 -0700

    adds bonding boxes to the dataset

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const parse = require("xml2js").parseString;
 
 async function main() {
  // console.log("hi");
@@ -19,14 +20,48 @@ async function main() {
   };
   for (let file of fs.readdirSync(`images/${dir}/`)) {
    // console.log(file);
-   entry.examples.push(`images/${dir}/${file}`);
+   let image = {
+    "@type": "Image",
+    "url": `images/${dir}/${file}`
+   };
+   entry.examples.push(image);
+   
+   // entry.examples.push(`images/${dir}/${file}`);
 
-   let annotations = fs.readFileSync(`annotations/${dir}/${file.substring(0, file.length - 4)}`);
-   console.log(annotations);
-   break;
+   let annotations = fs.readFileSync(`annotations/${dir}/${file.substring(0, file.length - 4)}`, "utf8");
+   // console.log(annotations);
+   let data = await new Promise(function(resolve, reject) {
+     parse(annotations, function(err, result) {
+       // console.log(JSON.stringify(result));
+       resolve(result);
+      });
+   });
+   // console.log(data.annotation.object);
+   if (data.annotation.size) {
+    image.size = {
+     width: data.annotation.size[0].width[0],
+     height: data.annotation.size[0].height[0],
+     depth: data.annotation.size[0].depth[0]
+    };
+   }
+   if (data.annotation.object) {
+    image.boxes = [];
+    for (let box of data.annotation.object[0].bndbox) {
+     // console.log(box);
+     image.boxes.push({
+       "@type": "Box",
+       "left": box.xmin[0],
+       "right": box.xmax[0],
+       "top": box.ymin[0],
+       "bottom": box.xmax[0]
+      });
+    }
+   }
+   
+   // break;
   }
   dataset.entries.push(entry);
-  break;
+  // break;
  }
  console.log(JSON.stringify(dataset, undefined, 2));
 }
